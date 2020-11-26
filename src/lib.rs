a5df024f0a4211cbdc7e3ce9bf7980ccfa2c06bf
Nils Eriksson <nils.edvin.eriksson@gmail.com>
2020-11-26 15:54:51 +0100

    Added comments

--- src/lib.rs
+++ src/lib.rs
@@ -2,63 +2,80 @@
 #![allow(clippy::manual_range_contains)]
 
 use bounded_integer::bounded_integer;
-use embedded_hal as hal;
-use hal::blocking::i2c::{Write, WriteRead};
+use embedded_hal::blocking::i2c::{Read, Write};
+
+pub const SEGMENTS_SIZE: usize  = 16;
+pub const COMMONS_SIZE: usize   = 8;
 
 pub mod command {
-    pub const DIMMING: u8 =     0b1110_0000;
-    pub const DISPLAY: u8 =     0b1000_0000;
-    pub const OSCILLATOR: u8 =  0b0010_0000;
+    pub const DIMMING: u8       = 0b1110_0000;
+    pub const DISPLAY: u8       = 0b1000_0000;
+    pub const OSCILLATOR: u8    = 0b0010_0000;
 }
 
 bounded_integer! {
-    #[repr(u8)]
-    pub enum Dimming { 0..16 }
+/// Holds the range of values a dimmer can have.
+/// Z0 being the lowest value and P15 being the largest.
+#[repr(u8)]
+pub enum Dimming { 0..16 }
 }
 
-#[derive(Copy, Clone)]
+// Holds display states
+#[derive(Copy, Clone, Debug, Hash)]
 pub enum Display {
-    Off =       0b0000_0000,
-    On =        0b0000_0001,
-    HalfHz =    0b0000_0111,
-    OneHz =     0b0000_0101,
-    TwoHz =     0b0000_0011,
+    Off     = 0b0000_0000,
+    On      = 0b0000_0001,
+    TwoHz   = 0b0000_0011,
+    OneHz   = 0b0000_0101,
+    HalfHz  = 0b0000_0111,
 }
 
-#[derive(Copy, Clone)]
+/// Holds oscillators 2 states
+#[derive(Copy, Clone, Debug, Hash)]
 pub enum Oscillator {
-    On = 1,
+    On  = 1,
     Off = 0,
 }
 
 type Result<Error> = core::result::Result<(), Error>;
 
+/// Main driver. Holds the state and buffer.
+#[derive(Copy, Clone, Debug, Hash)]
 pub struct HT16K33<I2C> {
-    i2c: I2C,
+    i2c:     I2C,
     address: u8,
 
-    pub buffer: [u8; 16],
+    pub buffer: [u8; SEGMENTS_SIZE],
 
-    dimming_level: Dimming,
-    display_state: Display,
+    dimming_level:    Dimming,
+    display_state:    Display,
     oscillator_state: Oscillator,
 }
 
 impl<I2C, E> HT16K33<I2C>
 where
-    I2C: Write<Error = E> + WriteRead<Error = E>
+    I2C: Read<Error = E> + Write<Error = E>
 {
+    /// Creates a new instance.
+    /// Should be followed by init().
     pub fn new(i2c: I2C, address: u8) -> Self {
         HT16K33 {
             address,
             i2c,
-            buffer: [0; 16],
-            dimming_level: Dimming::P15,
-            display_state: Display::Off,
+
+            buffer: [0; SEGMENTS_SIZE],
+
+            dimming_level:    Dimming::P15,
+            display_state:    Display::Off,
             oscillator_state: Oscillator::Off,
         }
     }
 
+    /// Turns on oscillator, display, turns dimming to max, 
+    /// clears and writes buffer.
+    /// 
+    /// These are just five linear steps, you can easily start your driver
+    /// diffrently.
     pub fn init(&mut self) -> Result<E> {
         self.write_oscillator(Oscillator::On)?;
         self.write_display(Display::On)?;
@@ -68,49 +85,68 @@ where
         self.write_buffer()
     }
 
+    /// Returns i2c interface.
     pub fn destroy(self) -> I2C {
         self.i2c
     }
 
+    /// This method allows for almost direct access to the i2c field, 
+    /// with only the address set. 
+    /// 
+    /// Use with caution! See the sourcecode of this library of how to use it.
+    pub fn write(&mut self, slice: &[u8]) -> Result<E> {
+        self.i2c.write(self.address, slice)
+    }
+
+    /// Clears the buffer without writing to controller.
+    /// Should perhaps be followed by `write_buffer()`
     pub fn clear_buffer(&mut self) {
-        self.buffer = [0; 16];
+        self.buffer = [0; SEGMENTS_SIZE];
     }
 
-    pub fn read_buffer(&mut self) -> Result<E> {
-        self.i2c.write_read(self.address, &[0], &mut self.buffer)
+    /// Reads in buffer from controller.
+    pub fn read_buffer(&mut self, buffer: &mut [u8; SEGMENTS_SIZE]) -> Result<E> {
+        self.i2c.read(self.address, buffer)
     }
 
+    /// Writes internal buffer to controller.
     pub fn write_buffer(&mut self) -> Result<E> {
-        let mut write_buffer = [0u8; 16 + 1];
+        let mut write_buffer = [0u8; SEGMENTS_SIZE + 1];
         write_buffer[0] = 0;
-        write_buffer[1..(16 + 1)].clone_from_slice(&self.buffer[..]);
-        self.i2c.write(self.address, &write_buffer)
+        write_buffer[1..].clone_from_slice(&self.buffer[..]);
+        self.write(&write_buffer)
     }
 
+    /// Return local dimming level. Might not reflect controller.
     pub fn dimming(&self) -> Dimming {
         self.dimming_level
     }
 
+    /// Writes a new dimming level to controller.
     pub fn write_dimming(&mut self, dimming: Dimming) -> Result<E> {
         self.dimming_level = dimming;
-        self.i2c.write(self.address, &[command::DIMMING | dimming as u8])
+        self.write(&[command::DIMMING | dimming as u8])
     }
 
+    /// Returns local display state. Might not reflect controller.
     pub fn display(&self) -> Display {
         self.display_state
     }
 
+    /// Writes a new display state to controller.
     pub fn write_display(&mut self, display: Display) -> Result<E> {
         self.display_state = display;
-        self.i2c.write(self.address, &[command::DISPLAY | display as u8])
+        self.write(&[command::DISPLAY | display as u8])
     }
 
+    /// Returns local oscillator state. Might not reflect controller.
     pub fn oscillator(&self) -> Oscillator {
         self.oscillator_state
     }
 
+    /// Writes a new oscillator state to controller.
     pub fn write_oscillator(&mut self, oscillator: Oscillator) -> Result<E> {
         self.oscillator_state = oscillator;
-        self.i2c.write(self.address, &[command::OSCILLATOR | oscillator as u8])
+        self.write(&[command::OSCILLATOR | oscillator as u8])
     }
 }
