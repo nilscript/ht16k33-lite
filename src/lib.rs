d75dfd0a2a27dedff93fbc953ff4707bf5a2db59
Nils Eriksson <nils.edvin.eriksson@gmail.com>
2021-06-08 11:43:19 +0200

    Made one field unsafe, corrected spelling on comments.

--- src/lib.rs
+++ src/lib.rs
@@ -21,11 +21,11 @@ pub mod commands {
     pub const TEST_MODE_HOLTEK_ONLY: u8         = 0b1101_1001; // Write only
 }
 
-/// Contains all legal states for each subsystem
+/// Contains all legal states for each subsystem.
 pub mod states {
     use bounded_integer::bounded_integer;
 
-    /// System Setup Register States
+    /// System Setup Register States.
     #[derive(Copy, Clone, Debug, Hash)]
     pub enum System {
         /// Oscillator off
@@ -34,7 +34,7 @@ pub mod states {
         Normal  = 1,
     }
 
-    /// ROW/INT Set Register States
+    /// ROW/INT Set Register States.
     #[derive(Copy, Clone, Debug, Hash)]
     pub enum RowInt {
         /// Row driver output
@@ -45,7 +45,7 @@ pub mod states {
         IntHigh = 0b0000_0011,
     }
 
-    /// Display Setup Register States
+    /// Display Setup Register States.
     #[derive(Copy, Clone, Debug, Hash)]
     pub enum Display {
         /// Display off
@@ -61,9 +61,9 @@ pub mod states {
     }
 
     bounded_integer! {
-    /// Digital Dimming Data Input Pulse Width Duties
+    /// Digital Dimming Data Input Pulse Width Duties.
     /// 
-    /// Ranges go from Z, P1..P15
+    /// Ranges go from Z, P1..P15.
     #[repr(u8)]
     pub enum Pulse { 0..16 }
     }
@@ -73,12 +73,12 @@ pub mod addresses {
     use bounded_integer::bounded_integer;
 
     bounded_integer! {
-    /// Display Data Address Pointer
+    /// Display Data Address Pointer.
     pub enum DDataAddress { 0..16 }
     }
 
     bounded_integer! {
-    /// Key Data Address Pointers
+    /// Key Data Address Pointers.
     pub enum KeyData { 0..6 }
     }
 }
@@ -88,10 +88,10 @@ use states::*;
 use addresses::*;
 type Result<Error> = core::result::Result<(), Error>;
 
-/// Driver for the HT16K33 RAM Mapping 16*8 LED controller Driver with keyscan
+/// Driver for the HT16K33 RAM Mapping 16*8 LED controller Driver with keyscan.
 /// 
-/// Has an internal state which it tries to reflec 
-/// onto connected micromicrocontroller.
+/// Has an internal state which it tries to reflect
+/// onto the connected controller.
 /// 
 /// Internal buffer is exposed and should be read and edited that way.
 /// Write buffer with 
@@ -113,7 +113,7 @@ where
     I2C: Read<Error = E> + Write<Error = E>
 {
     /// Creates a new instance.
-    /// Should be followed by init().
+    /// Should be followed by power_on().
     pub fn new(i2c: I2C, address: u8) -> Self {
         HT16K33 {
             address,
@@ -130,8 +130,6 @@ where
 
     /// Turns system to normal mode, set's Row/Int to Row, turns on display, 
     /// turns dimming to max, and writes a blank dbuffer.
-    /// 
-    /// These are just five linear steps, you can easily start it diffrently.
     pub fn power_on(&mut self) -> Result<E> {
         self.write_system(System::Normal)?;
         self.write_rowint(RowInt::Row)?;
@@ -142,7 +140,7 @@ where
 
     /// Writes blank buffer, turns off dimming display and oscillator.
     /// 
-    /// Almost the reverse off `init()`.
+    /// Almost the reverse off `power_on()`.
     pub fn shutdown(&mut self) -> Result<E> {
         self.write_dbuf()?;
         self.write_dimming(Pulse::MAX)?;
@@ -153,11 +151,10 @@ where
     /// Destroys self and returns internal i2c interface.
     pub fn destroy(self) -> I2C {self.i2c}
 
-    /// This method allows for almost direct access to the i2c field, 
-    /// with only the address set. 
+    /// Writes unchecked slice to controller.
     /// 
     /// Use with caution! See the sourcecode of this library of how to use it.
-    pub fn write(&mut self, slice: &[u8]) -> Result<E> {
+    pub unsafe fn write(&mut self, slice: &[u8]) -> Result<E> {
         self.i2c.write(self.address, slice)
     }
     
@@ -168,18 +165,18 @@ where
     /// Writes new System mode to controller
     /// and if successful store it's new state.
     pub fn write_system(&mut self, mode: System) -> Result<E> {
-        self.write(&[SYSTEM_SETUP | mode as u8])?;
+        unsafe { self.write(&[SYSTEM_SETUP | mode as u8])?; }
         self.system = mode; Ok(())
     }
 
     /// Returns reference to internal display state. 
-    /// Might not reflect microcontroller.
+    /// Might not reflect controller.
     pub fn display(&self) -> Display {self.display}
 
-    /// Writes a new display state to microcontroller 
+    /// Writes a new display state to controller 
     /// and if successful store it's new state.
     pub fn write_display(&mut self, display: Display) -> Result<E> {
-        self.write(&[DISPLAY_SETUP | display as u8])?;
+        unsafe { self.write(&[DISPLAY_SETUP | display as u8])?; }
         self.display = display; Ok(())
     }
 
@@ -190,7 +187,7 @@ where
     /// Writes new Row/Int output to controller
     /// and if successful store it's new state.
     pub fn write_rowint(&mut self, rowint: RowInt) -> Result<E> {
-        self.write(&[ROWINT_SET | rowint as u8])?;
+        unsafe { self.write(&[ROWINT_SET | rowint as u8])?; }
         self.rowint = rowint; Ok(())
     }
     
@@ -201,7 +198,7 @@ where
     /// Writes a new dimming level to controller
     /// and if successful store it's new state.
     pub fn write_dimming(&mut self, pulse: Pulse) -> Result<E> {
-        self.write(&[DIMMING_SET | pulse as u8])?;
+        unsafe { self.write(&[DIMMING_SET | pulse as u8])?; }
         self.dimming = pulse; Ok(())
     }
 
@@ -214,7 +211,7 @@ where
     pub fn write_dbuf(&mut self) -> Result<E> {
         let mut write_buffer = [0; SEGMENTS_SIZE + 1];
         write_buffer[1..].clone_from_slice(&self.dbuf);
-        self.write(&write_buffer)
+        unsafe { self.write(&write_buffer) }
     }
 
     /// Reads Display Ram from controller into buffer.
@@ -231,6 +228,6 @@ where
         let mut write_buffer = [0; SEGMENTS_SIZE + 1];
         write_buffer[0] = *address as u8;
         write_buffer[1..].clone_from_slice(slice);
-        self.write(&write_buffer)
+        unsafe { self.write(&write_buffer) }
     }
 }
