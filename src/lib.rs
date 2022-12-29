2bf9b16027943da7833c6cb470740ca0349db6c7
Nils Eriksson <nils.edvin.eriksson@gmail.com>
2022-12-29 22:24:24 +0100

    Rewrote how display buffer is stored. Updated dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(clippy::manual_range_contains)]
 #![cfg_attr(not(feature = "std"), no_std)]
-#![feature(step_trait, step_trait_ext)]
+#![feature(step_trait)]
 
 //! 
 
@@ -41,7 +41,7 @@ pub mod states {
         Row     = 0b0000_0000,
         /// Int output, active low
         IntLow  = 0b0000_0001,
-        /// Int ooutput, active high
+        /// Int output, active high
         IntHigh = 0b0000_0011,
     }
 
@@ -63,9 +63,9 @@ pub mod states {
     bounded_integer! {
     /// Digital Dimming Data Input Pulse Width Duties.
     /// 
-    /// Ranges go from Z, P1..P15.
+    /// Ranges go from Z, P1..=P15.
     #[repr(u8)]
-    pub enum Pulse { 0..16 }
+    pub enum Pulse { 0..=15 }
     }
 }
 
@@ -88,7 +88,7 @@ use states::*;
 use addresses::*;
 type Result<Error> = core::result::Result<(), Error>;
 
-/// Driver for the HT16K33 RAM Mapping 16*8 LED controller Driver with keyscan.
+/// Driver for the HT16K33 RAM Mapping 16*8 LED controller Driver with key scan.
 /// 
 /// Has an internal state which it tries to reflect
 /// onto the connected controller.
@@ -97,11 +97,9 @@ type Result<Error> = core::result::Result<(), Error>;
 /// Write buffer with 
 #[derive(Copy, Clone, Debug, Hash)]
 pub struct HT16K33<I2C> {
-    i2c:     I2C,
-    address: u8,
-
-    pub dbuf: [u8; SEGMENTS_SIZE],
-
+    i2c:      I2C,
+    address:  u8,
+    dbuf:     [u8; SEGMENTS_SIZE + 1],
     system:   System,
     display:  Display,
     rowint:   RowInt,
@@ -116,11 +114,9 @@ where
     /// Should be followed by power_on().
     pub fn new(i2c: I2C, address: u8) -> Self {
         HT16K33 {
-            address,
             i2c,
-
-            dbuf: [0; SEGMENTS_SIZE],
-
+            address,
+            dbuf:     [0; SEGMENTS_SIZE + 1],
             system:   System::StandBy,
             display:  Display::Off,
             rowint:   RowInt::Row,
@@ -129,19 +125,20 @@ where
     }
 
     /// Turns system to normal mode, set's Row/Int to Row, turns on display, 
-    /// turns dimming to max, and writes a blank dbuffer.
+    /// turns dimming to max, and writes what is in display buffer.
     pub fn power_on(&mut self) -> Result<E> {
         self.write_system(System::Normal)?;
         self.write_rowint(RowInt::Row)?;
         self.write_display(Display::On)?;
-        self.write_dimming(Pulse::P15)?;
+        self.write_dimming(Pulse::MAX)?;
         self.write_dbuf()
     }
 
-    /// Writes blank buffer, turns off dimming display and oscillator.
+    /// Clears and writes a bland display buffer, turns off dimming display and oscillator.
     /// 
     /// Almost the reverse off `power_on()`.
     pub fn shutdown(&mut self) -> Result<E> {
+        self.clear_dbuf();
         self.write_dbuf()?;
         self.write_dimming(Pulse::MAX)?;
         self.write_display(Display::Off)?;
@@ -153,9 +150,19 @@ where
 
     /// Writes unchecked slice to controller.
     /// 
-    /// Use with caution! See the sourcecode of this library of how to use it.
+    /// Use with caution! See the source code of this library of how to use it.
     pub unsafe fn write(&mut self, slice: &[u8]) -> Result<E> {
         self.i2c.write(self.address, slice)
+    }
+
+    /// Returns a copy of the address.
+    pub fn address(&self) -> u8 {
+        self.address
+    }
+
+    /// Sets the address.
+    pub fn set_address(&mut self, address: u8) {
+        self.address = address;
     }
     
     /// Returns reference to internat system mode. 
@@ -202,21 +209,41 @@ where
         self.dimming = pulse; Ok(())
     }
 
+    /// Returns display buffer as a slice.
+    pub fn dbuf(&self) -> &[u8] {
+        &self.dbuf[1..]
+    }
+
+    /// Returns display buffer as a mutable slice.
+    pub fn dbuf_mut(&mut self) -> &mut [u8] {
+        &mut self.dbuf[1..]
+    }
+
+    /// Sets the display buffer.
+    ///
+    /// The length of `buf` must be `SEGMENTS_SIZE`.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if the length of `buf` differs.
+    pub fn set_dbuf(&mut self, buf: &[u8]) {
+        self.dbuf[1..].clone_from_slice(&buf)
+    }
+
     /// Clears Display Buffer.
     pub fn clear_dbuf(&mut self) {
-        self.dbuf = [0; SEGMENTS_SIZE];
+        self.dbuf[1..].fill(0);
     }
 
     /// Writes Display Buffer to controller Display Ram.
     pub fn write_dbuf(&mut self) -> Result<E> {
-        let mut write_buffer = [0; SEGMENTS_SIZE + 1];
-        write_buffer[1..].clone_from_slice(&self.dbuf);
-        unsafe { self.write(&write_buffer) }
+        self.i2c.write(self.address, &self.dbuf)
     }
 
-    /// Reads Display Ram from controller into buffer.
-    pub fn read_dram(&mut self, buf: &mut [u8]) -> Result<E> {
-        self.i2c.read(self.address, buf)
+    /// Reads Display Ram from controller into a provided buffer or 
+    /// internal display buffer.
+    pub fn read_dram(&mut self, buf: Option<&mut [u8]>) -> Result<E> {
+        self.i2c.read(self.address, buf.unwrap_or(&mut self.dbuf[1..]))
     }
 
     /// Writes slice to controller Display Ram 
